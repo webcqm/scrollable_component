74299dc53792ec37004d9764eae55f8374c97c9d
webcqm <2650731512@qq.com>
2022-12-17 17:00:54 +0800

    update

--- lib/SingleChildScrollViewTestRoute.dart
+++ lib/SingleChildScrollViewTestRoute.dart
@@ -11,7 +11,7 @@ class SingleChildScrollViewTestRoute extends StatelessWidget {
           child: Column(
             //动态创建一个List<Widget>
             children: str.split("")
-            //每一个字母都用一个Text显示,字体为原来的两倍
+            //每一个字母都用一个Text显示,字体为原来的两倍.
                 .map((c) => Text(c, textScaleFactor: 2.0,))
                 .toList(),
           ),
